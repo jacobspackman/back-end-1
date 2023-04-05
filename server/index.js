0b481af3529e35ea03926cc0521924590bca81d1
Jacob Spackman <jacobts202@gmail.com>
2023-04-04 18:45:21 -0600

    lab complete

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/api/users", (req, res) => {
     let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
